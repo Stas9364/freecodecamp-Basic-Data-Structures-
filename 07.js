7db180ac9b6a349ef63a9c70bf0939de95f6fd0f
Stanislav Lisovskiy <86848801+Stas9364@users.noreply.github.com>
2022-03-13 14:21:48 +0300

    Add files via upload

---
+++ 07.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+    // Only change code below this line
+  let x = arr.slice(2, 4);
+    return x;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
